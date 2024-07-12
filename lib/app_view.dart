aaccfd94e6e8e055e12aa750439064c8df654af8
Hezio Cumba <heziocumba@gmail.com>
2024-07-13 00:18:08 +0200

    feat:creating the authentication feature

---
+++ lib/app_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_app/app.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Pizza Delivery",
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pizza_app/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(const MyApp(FirebaseUserRepo()));
 }
