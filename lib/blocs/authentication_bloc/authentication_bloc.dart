4ef7a6194553ad26a942b21b67babd239b6b8889
Ndhambi Osvaldo <osvaldondhambi@gmail.com>
2024-07-24 19:41:11 +0200

    authentication_bloc added

---
+++ lib/blocs/authentication_bloc/authentication_bloc.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:user_repository/user_repository.dart';
+
+part 'authentication_event.dart';
+part 'authentication_state.dart';
+
+class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+  late final StreamSubscription<MyUser?> _userSubsctription;
+
+  AuthenticationBloc({
+    required this.userRepository
+  }) : super(const AuthenticationState.unknown()) {
+    _userSubsctription = userRepository.user.listen((myUser){
+      add(AuthenticationUserChanged(myUser));
+    });
+
+    on<AuthenticationUserChanged>((event, emit) {
+      if(event.user != MyUser.empty) {
+        emit(AuthenticationState.authenticated(event.user!));
+      } else{
+        emit(AuthenticationState.unauthenticated());
+      }
+    });
+  }
+
+  @override
+  Future<void> close() {
+    _userSubsctription.cancel();
+    return super.close();
+  }
+}
